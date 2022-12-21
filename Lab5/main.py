9ff1349238c7f3f1183763ea234b958f7409e94f
AndreiLupsa <72352373+AndreiLupsa@users.noreply.github.com>
2022-12-21 16:37:58 +0200

    1

--- Lab5/main.py
+++ Lab5/main.py
@@ -3,8 +3,8 @@ from Parser import ParserRecursiveDescendent
 
 pr = ParserRecursiveDescendent(
     "C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFCD\\Lab5\\g1.txt",
-    "C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFCD\\Lab5\\seq.txt",
-    "C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFCD\\Lab5\\myout.txt")
+    "C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFCD\\seq.txt",
+    "C:\\Users\\andre\\OneDrive\\Documents\\GitHub\\LFCD\\Lab5\\out1.txt")
 pr.run(pr.sequence)
 
 print(str(pr))
